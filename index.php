2bf58f7b6cc355f1b8057f6d8393f2a8aa07322d
Melissa De Alba <Rubi_93_22@hotmail.com>
2017-06-08 22:59:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
 <?php
 	
-
-	class trinagle {
+class trinagle {
 
 		public function startTrinagle()
 		{
 			$methodService = $_SERVER['REQUEST_METHOD'];
 
 			if ($methodService== "GET") {
-				
-				 $this->validateTriangle($_SERVER['REQUEST_URI']);
+				$result = $this->validateTriangle();
+				echo json_encode($result);
 			}
 			
 			
 		}
 
-		private function validateTriangle($data){
-			var_dump($data);
+		private function validateTriangle(){
+			$a = 1;
+			$b = 2;
+			$c = 3;
+			if (($a+$b > $c) && ($b+$c > $a) && ($a+$c > $b)) {
+				$response = array(
+        			'Resultado' => 'Si es triangulo.'
+    			);
+			} else {
+				$response = array(
+        			'Resultado' => 'No es triangulo.'
+    			);			
+			}
+			return $response;
 		}
 	}
 
 	$trinagle = new trinagle;
 	$trinagle->startTrinagle();
-	echo "DevDay4Women";
 ?>
